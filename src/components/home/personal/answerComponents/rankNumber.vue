ac1485ed286f9e57cca51cc54542137b29de9f87
Mrgao1998 <1094210277@qq.com>
2020-10-19 21:49:45 +0800

    封装排行榜不同名次显示不同图标的组件

---
+++ src/components/home/personal/answerComponents/rankNumber.vue
@@ -0,0 +1,31 @@
+<template>
+  <van-col span="4" class="ranking" v-if="num === 1"><img src="./first.png" ></van-col>
+  <van-col span="4" class="ranking" v-else-if="num === 2"><img src="./second.png"></van-col>
+  <van-col span="4" class="ranking" v-else-if="num === 3"><img src="./third.png"></van-col>
+  <van-col span="4" class="ranking" v-else>{{ num }}</van-col>
+</template>
+
+<script>
+import Vue from "vue"
+import {Col} from "vant"
+Vue.use(Col)
+export default {
+  name: "rankNumber",
+  props: {
+    num: Number
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+<style scoped lang="stylus">
+  .ranking
+    color #ffcd32
+    font-size 18px
+    font-weight bold
+    img
+      width 30px
+      height 30px
+      margin 10px
+</style>
